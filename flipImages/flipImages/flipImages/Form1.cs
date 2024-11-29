51a4ec48e9b65ff6c6cebc0140d74bc236300444
NastiaGergil <gergilmo11@gmail.com>
2024-11-29 14:14:16 +0200

    first commit

---
+++ flipImages/flipImages/flipImages/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.WebRequestMethods;
+
+namespace flipImages
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap img;
+        private string imgPath;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void button1_Click_Find(object sender, EventArgs e)
+        {
+            FolderBrowserDialog folderDialog = new FolderBrowserDialog();
+            if (folderDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFolder = folderDialog.SelectedPath;
+
+                string[] files = Directory.GetFiles(selectedFolder.ToString());
+                
+                listBox1.DataSource= files;
+            }
+        }
+
+        public void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+            imgPath = listBox1.SelectedItem.ToString();
+            try
+            {
+                img = new Bitmap(imgPath);
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Image = img;
+            }
+            catch
+            {
+                MessageBox.Show("Не вдалося відкрити зображення (можливо вибраний файл не є зображенням).", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void button2_Click_Flip(object sender, EventArgs e)
+        {
+            try
+            {
+                img.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox1.Image = img;
+            }
+            catch(NullReferenceException)
+            {
+                MessageBox.Show("Виберіть зображення для відзеркалення.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void button3_Click_Save(object sender, EventArgs e)
+        {
+            /*if (img != null && !string.IsNullOrEmpty(imgPath))
+            {
+                try
+                {
+                    string directory = Path.GetDirectoryName(imgPath);
+                    string fileName = Path.GetFileNameWithoutExtension(imgPath);
+                    string saveImgPath = Path.Combine(directory, fileName);
+
+                    img.Save($"{saveImgPath} - mirrored.gif");
+                    MessageBox.Show($"Зображення збережено як: {saveImgPath} - mirrored.gif", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                catch
+                {
+                    MessageBox.Show("Не вдалося зберегти зображення (можливо відзеркалене зображення вже збережено).", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ви не вибрали зображення для збереження.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }*/
+
+
+            try
+            {
+                string directory = Path.GetDirectoryName(imgPath);
+                string fileName = Path.GetFileNameWithoutExtension(imgPath);
+                string saveImgPath = Path.Combine(directory, fileName);
+                
+                    img.Save($"{saveImgPath} - mirrored.gif");
+                    MessageBox.Show($"Зображення збережено як: {saveImgPath} - mirrored.gif", "Успіх", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch(ExternalException)
+            {
+                    MessageBox.Show($"Не вдалося зберегти зображення (можливо вже існує файл з таким ім'ям).", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (ArgumentException)
+            {
+                MessageBox.Show("Або ви не вибрали зображення, або шлях до файлу пошкоджено.", "Помилка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ lab_exceptions/exceptions/exceptions/Program.cs
@@ -0,0 +1,77 @@
+﻿namespace exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double sum = 0;
+            int count = 0;
+
+            List<string> noFile = new List<string>();
+            List<string> badData = new List<string>();
+            List<string> overflow = new List<string>();
+            List<int> validProducts = new List<int>();
+
+            try
+            {
+                string folderPath = @"C:\Users\Lenovo\Desktop\Proba\createFiles";
+
+                string[] filenames = new string[20];
+                for (int i = 0; i < 20; i++)
+                {
+                    filenames[i] = $"{10 + i}.txt";
+
+                    try
+                    {
+                        string filePath = Path.Combine(folderPath, $"{filenames[i]}.txt");
+                        string[] lines = File.ReadAllLines(filePath);
+                        int num1 = int.Parse(lines[0].Trim());
+                        int num2 = int.Parse(lines[1].Trim());
+
+                        try
+                        {
+                            checked
+                            {
+                                int multiply = num1 * num2;
+                                validProducts.Add(multiply);
+                                sum += multiply;
+                                count++;
+                            }
+                        }
+                        catch (OverflowException)
+                        {
+                            overflow.Add(filenames[i]);
+                        }
+                    }
+                    catch (FileNotFoundException)
+                    {
+                        noFile.Add(filenames[i]);
+                    }
+                    catch (Exception)
+                    {
+                        badData.Add(filenames[i]);
+                    }
+                }
+
+                File.WriteAllLines(Path.Combine(folderPath, "no_file.txt"), noFile);
+                File.WriteAllLines(Path.Combine(folderPath, "bad_data.txt"), badData);
+                File.WriteAllLines(Path.Combine(folderPath, "overflow.txt"), overflow);
+
+                try
+                {
+                    double average = sum / count;
+                    Console.WriteLine($"Середнє арифметичне: {average}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Помилка створення або запису у файли результатів.");
+                return;
+            }
+        }
+    }
+}
